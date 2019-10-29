9269f1eaf3695eba775b9464b67bd5b15a01486a
Szab <contact@szabolcsszucs.com>
2019-10-29 10:56:43 +0100

    returning principal on test GET request

--- src/main/java/com/kodekonveyor/tidyup/AuthenticatedTestController.java
+++ src/main/java/com/kodekonveyor/tidyup/AuthenticatedTestController.java
@@ -5,15 +5,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class AuthenticatedTestController {
     @GetMapping("/test")
     @ResponseStatus(value = HttpStatus.OK)
-    public void test(final HttpServletRequest request, final HttpServletResponse response) {
-    	
+    public @ResponseBody String test() {
+    	return SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
     }
 }
