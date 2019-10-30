4e4f29b09061b2d4ca15974ae59c248c5bc4c95b
Szab <contact@szabolcsszucs.com>
2019-10-30 20:42:24 +0100

    first approximation of tests

---
+++ src/test/java/com/kodekonveyor/tidyup/TidyUserControllerTest.java
@@ -0,0 +1,162 @@
+package com.kodekonveyor.tidyup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.persistence.Tuple;
+
+import org.assertj.core.util.Arrays;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.MultiValueMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Data;
+import lombok.Setter;
+
+// @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("tidyuser behaviour")
+public class TidyUserControllerTest {
+	private static final Logger logger = LoggerFactory.getLogger(TidyUserControllerTest.class);
+	
+    @Autowired
+    private TestRestTemplate restTemplate;
+    
+    @LocalServerPort
+    Integer port;
+    
+    private class U {
+    	public String self;
+    	public HttpEntity<TidyUserDto> u;
+    };
+    
+    private U createCustomer(Optional<String> email) {
+        TidyUserDto u1 = new TidyUserDto();
+        u1.setEmail(email.orElse(UUID.randomUUID().toString()+"@test.com"));
+        u1.setRole(RoleDto.CUSTOMER);
+        u1.setPassword("test1");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<TidyUserDto> re = new HttpEntity<TidyUserDto>(u1,headers);
+        
+        ResponseEntity<TidyUserResource> r1 = restTemplate.postForEntity("/users", re, TidyUserResource.class);
+        logger.info(r1.toString());
+        
+        assertThat(r1.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        String self = r1.getBody().getLink("self").getHref();
+        U r = new U();
+        r.self = self.replaceAll("^http://localhost:"+this.port.toString(), "");
+        r.u = re;
+        return r;
+    }
+    
+    private U registerCustomer() {
+    	return createCustomer(Optional.empty());
+    }
+    
+	    @Test
+	    public void registerThenRequery() {
+	    	U u = registerCustomer();
+	    	ResponseEntity<TidyUserResource> r = restTemplate.withBasicAuth(u.u.getBody().getEmail(), u.u.getBody().getPassword()).getForEntity(u.self,TidyUserResource.class);
+	        assertThat(r.getStatusCode()).isEqualTo(HttpStatus.OK);
+	    }
+	    
+	    @Test
+	    public void alreadyRegistered() {
+	    	U u = registerCustomer();
+	    	ResponseEntity<VndErrors> r = restTemplate.postForEntity("/users", u.u, VndErrors.class);
+	    	assertThat(r.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+	    }
+	    
+	    @Test
+	    public void usersSeeEachothersProfile() {
+	    	U u1 = registerCustomer();
+	    	U u2 = registerCustomer();
+	    	ResponseEntity<TidyUserResource> r = restTemplate.withBasicAuth(u1.u.getBody().getEmail(), u1.u.getBody().getPassword()).getForEntity(u2.self,TidyUserResource.class);
+	        assertThat(r.getStatusCode()).isEqualTo(HttpStatus.OK);
+	    }
+	    
+	    private class W {
+	    	public WorkRequestDto wrequest;
+	    	public String self;
+	    }
+	    
+	    private W addWorkRequest(U u) {
+	    	WorkRequestDto req = new WorkRequestDto();
+	    	req.setCity("AOEU");
+	    	req.setDescription("Want my space to be tidy.");
+	    	ResponseEntity<WorkRequestResource> res = restTemplate.withBasicAuth(u.u.getBody().getEmail(), u.u.getBody().getPassword()).postForEntity(u.self+"/workrequests", req, WorkRequestResource.class);
+	    	
+	    	assertThat(res.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+	        String self = res.getBody().getLink("self").getHref();
+	        W r = new W();
+	        r.self = self.replaceAll("^http://localhost:"+this.port.toString(), "");
+	        r.wrequest = req;
+	        return r;
+	    }
+	    
+	    @Test
+	    public void userCanAddWorkRequest() {
+	    	U u = registerCustomer();
+	    	addWorkRequest(u);
+	    }
+
+	    @Test
+	    public void userCanRequeryWorkRequest() {
+	    	U u = registerCustomer();
+	    	W w = addWorkRequest(u);
+	    	ResponseEntity<WorkRequestResource> r = restTemplate.withBasicAuth(u.u.getBody().getEmail(), u.u.getBody().getPassword()).getForEntity(w.self,WorkRequestResource.class);
+	        assertThat(r.getStatusCode()).isEqualTo(HttpStatus.OK);
+	    }
+	    
+	    @Test
+	    public void userCanDeleteWorkRequest() {
+	    	U u = registerCustomer();
+	    	W w = addWorkRequest(u);
+	    	ResponseEntity<Void> r = restTemplate.withBasicAuth(u.u.getBody().getEmail(), u.u.getBody().getPassword()).exchange("http://localhost:"+port.toString()+w.self, HttpMethod.DELETE, null, Void.class);
+	        assertThat(r.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+	        ResponseEntity<Object> gone = restTemplate.withBasicAuth(u.u.getBody().getEmail(), u.u.getBody().getPassword()).getForEntity(w.self,Object.class);
+	        logger.info(gone.toString());
+	        assertThat(gone.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	    }
+}
