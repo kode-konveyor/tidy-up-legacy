3babe1b4848f6c43b2c5bd18abcfa99fc75a2a77
Szab <contact@szabolcsszucs.com>
2019-10-30 21:43:44 +0100

    removing unused imports

--- src/main/java/com/kodekonveyor/tidyup/TidyUserDto.java
+++ src/main/java/com/kodekonveyor/tidyup/TidyUserDto.java
@@ -1,10 +1,5 @@
 package com.kodekonveyor.tidyup;
 
-import java.util.Collection;
-
-import javax.persistence.Entity;
-
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
