0cf57671619ea929569948f72285061d54a7e9e4
Szab <contact@szabolcsszucs.com>
2019-10-30 08:55:23 +0100

    missed the new exception file

---
+++ src/main/java/com/kodekonveyor/tidyup/TidyUserAlreadyRegisteredException.java
@@ -0,0 +1,15 @@
+package com.kodekonveyor.tidyup;
+
+import lombok.Getter;
+
+@Getter
+public class TidyUserAlreadyRegisteredException extends RuntimeException {
+	
+	  private final String email;
+
+	  public TidyUserAlreadyRegisteredException(final String email) {
+	    super("TidyUser is already registered w/ email: " + email);
+	    this.email = email;
+	}
+
+}
