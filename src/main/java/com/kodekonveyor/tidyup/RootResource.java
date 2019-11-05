11a647ff2128e74d555364bbd31f226281d9e89d
Szab <contact@szabolcsszucs.com>
2019-11-05 16:11:18 +0100

    extracting one more literal to const

--- src/main/java/com/kodekonveyor/tidyup/RootResource.java
+++ src/main/java/com/kodekonveyor/tidyup/RootResource.java
@@ -6,8 +6,10 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 public class RootResource extends ResourceSupport {
+	private static final String ALL_USERS = "all-users";
+
 	public RootResource() {
-		add(linkTo(TidyUserController.class).withRel("all-users"));
+		add(linkTo(TidyUserController.class).withRel(ALL_USERS));
 		add(linkTo(methodOn(RootController.class).root()).withSelfRel());
 	}
 }
