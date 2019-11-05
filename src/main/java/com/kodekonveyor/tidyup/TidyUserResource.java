20deee0ca39030ddb81164f1b7eed53a39596771
Szab <contact@szabolcsszucs.com>
2019-11-05 10:08:23 +0100

    extracting literals to constants

--- src/main/java/com/kodekonveyor/tidyup/TidyUserResource.java
+++ src/main/java/com/kodekonveyor/tidyup/TidyUserResource.java
@@ -9,10 +9,13 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @NoArgsConstructor
 public class TidyUserResource extends ResourceSupport {
+	private static final String USER_WORKREQUESTS = "user-workrequests";
+	private static final String ALL_USERS = "all-users";
+
 	public TidyUserResource(final TidyUser user) {
 		final long id = user.getId();
-		add(linkTo(TidyUserController.class).withRel("all-users"));
-		add(linkTo(methodOn(WorkRequestsController.class).all(id)).withRel("user-workrequests"));
+		add(linkTo(TidyUserController.class).withRel(ALL_USERS));
+		add(linkTo(methodOn(WorkRequestsController.class).all(id)).withRel(USER_WORKREQUESTS));
 		add(linkTo(methodOn(TidyUserController.class).get(id)).withSelfRel());
 	}
 }
--- src/main/java/com/kodekonveyor/tidyup/WorkRequestResource.java
+++ src/main/java/com/kodekonveyor/tidyup/WorkRequestResource.java
@@ -13,6 +13,11 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @NoArgsConstructor
 public class WorkRequestResource extends ResourceSupport {
 
+	private static final String ALL_REQUESTS_FROM_CITY = "all-requests-from-city";
+	private static final String OWNER = "owner";
+	private static final String ALL_OWNER_REQUESTS = "all-owner-requests";
+	private static final String REQUEST_ID = "request-id";
+	
 	private WorkRequest workRequest;
 
 	public WorkRequestResource(final WorkRequest workRequest) {
@@ -20,10 +25,10 @@ public class WorkRequestResource extends ResourceSupport {
 		final long id = workRequest.getId();
 		final long userId = workRequest.getUser().getId();
 		final String city = workRequest.getCity();
-		add(new Link(String.valueOf(id), "request-id"));
-		add(linkTo(methodOn(WorkRequestsController.class).all(userId)).withRel("all-owner-requests"));
-		add(linkTo(methodOn(TidyUserController.class).get(userId)).withRel("owner"));
-		add(linkTo(methodOn(WorkRequestsController.class).city(city)).withRel("all-requests-from-city"));
+		add(new Link(String.valueOf(id), REQUEST_ID));
+		add(linkTo(methodOn(WorkRequestsController.class).all(userId)).withRel(ALL_OWNER_REQUESTS));
+		add(linkTo(methodOn(TidyUserController.class).get(userId)).withRel(OWNER));
+		add(linkTo(methodOn(WorkRequestsController.class).city(city)).withRel(ALL_REQUESTS_FROM_CITY));
 		add(linkTo(methodOn(WorkRequestsController.class).get(userId, id)).withSelfRel());
 	}
 
