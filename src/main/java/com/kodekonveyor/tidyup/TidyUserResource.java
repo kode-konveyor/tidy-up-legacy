1697ec3c36c5081a572539accb9595d4285a8fe9
Szab <contact@szabolcsszucs.com>
2019-11-05 10:03:21 +0100

    removing unnecessary code/field

--- src/main/java/com/kodekonveyor/tidyup/TidyUserResource.java
+++ src/main/java/com/kodekonveyor/tidyup/TidyUserResource.java
@@ -1,21 +1,15 @@
 package com.kodekonveyor.tidyup;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import org.springframework.hateoas.ResourceSupport;
+
+import lombok.NoArgsConstructor;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-@Getter
 @NoArgsConstructor
 public class TidyUserResource extends ResourceSupport {
-
-	private TidyUser user;
-
 	public TidyUserResource(final TidyUser user) {
-		this.user = user;
 		final long id = user.getId();
 		add(linkTo(TidyUserController.class).withRel("all-users"));
 		add(linkTo(methodOn(WorkRequestsController.class).all(id)).withRel("user-workrequests"));
