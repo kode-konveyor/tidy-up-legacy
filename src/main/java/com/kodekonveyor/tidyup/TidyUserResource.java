eed2847eb2f541e29360de6f14c5020b19354d2f
Szab <contact@szabolcsszucs.com>
2019-11-05 15:03:56 +0100

    based on feedback, thinking through default constructor of TidyUserResource

--- src/main/java/com/kodekonveyor/tidyup/TidyUserResource.java
+++ src/main/java/com/kodekonveyor/tidyup/TidyUserResource.java
@@ -2,15 +2,16 @@ package com.kodekonveyor.tidyup;
 
 import org.springframework.hateoas.ResourceSupport;
 
-import lombok.NoArgsConstructor;
-
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-@NoArgsConstructor
 public class TidyUserResource extends ResourceSupport {
 	private static final String USER_WORKREQUESTS = "user-workrequests";
 	private static final String ALL_USERS = "all-users";
+	
+	public TidyUserResource() {
+		add(linkTo(TidyUserController.class).withRel(ALL_USERS));
+	}
 
 	public TidyUserResource(final TidyUser user) {
 		final long id = user.getId();
