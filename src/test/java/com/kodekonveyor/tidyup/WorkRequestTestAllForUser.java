41ffb0fc63c13a2005c9d695e36bced62affeaa7
Szabolcs Szucs <contact@szabolcsszucs.com>
2019-12-03 20:00:40 +0100

    fixed mutation coverage for WorkRequestResource

--- src/test/java/com/kodekonveyor/tidyup/WorkRequestTestAllForUser.java
+++ src/test/java/com/kodekonveyor/tidyup/WorkRequestTestAllForUser.java
@@ -4,21 +4,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class WorkRequestTestAllForUser extends WorkRequestTestBase {
-	@Test
-	public void call() {
+	private WorkRequestResource get() {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
 
 		when(tidyUserRepository.findById(USER_IDENTIFIER)).thenReturn(user());
-		ResponseEntity<Resources<WorkRequestResource>> response = workRequestController.all(USER_IDENTIFIER);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		return workRequestController.all(USER_IDENTIFIER).getBody().getContent().iterator().next();
+
+	}
+
+	@Test
+	public void hasSelfLink() {
+		assertThat(get().getLink("self").getRel()).isEqualTo("self");
+	}
+
+	@Test
+	public void hasOwnerLink() {
+		assertThat(get().getLink("owner").getRel()).isEqualTo("owner");
+	}
+
+	@Test
+	public void hasAllOwnerRequest() {
+		assertThat(get().getLink("all-owner-requests").getRel()).isEqualTo("all-owner-requests");
+	}
+
+	@Test
+	public void hasAllFromCity() {
+		assertThat(get().getLink("all-requests-from-city").getRel()).isEqualTo("all-requests-from-city");
+	}
+
+	@Test
+	public void hasRequestId() {
+		assertThat(get().getLink("request-id").getRel()).isEqualTo("request-id");
 	}
 }
