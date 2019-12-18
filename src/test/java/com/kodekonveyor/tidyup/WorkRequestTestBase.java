d0d8928572db89988f2aa0702b91292f0da40f88
Szabolcs Szucs <contact@szabolcsszucs.com>
2019-12-18 21:12:11 +0100

    revert renames of test base classes

--- src/test/java/com/kodekonveyor/tidyup/WorkRequestTestBase.java
+++ src/test/java/com/kodekonveyor/tidyup/WorkRequestTestBase.java
@@ -3,7 +3,7 @@ package com.kodekonveyor.tidyup;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 
-public class WorkRequestBaseTest extends TestBase {
+public class WorkRequestTestBase extends TestBase {
 	protected final TidyUserRepository tidyUserRepository = Mockito.mock(TidyUserRepository.class);
 	protected final WorkRequestRepository workRequestRepository = Mockito.mock(WorkRequestRepository.class);
 
